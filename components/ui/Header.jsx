970bdc35a86d782a9c39f2aa24683428f3d66d1a
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-04-14 13:57:22 -0400

    latest update

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,22 +1,15 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import { Button } from "./button";
-import {
-  CircleFadingPlus,
-  FileCode2,
-  FileUser,
-  HomeIcon,
-  Layers,
-  LucideUserRound,
-  Mail,
-  Plus,
-  UserRoundPlus,
-} from "lucide-react";
+import { FileCode2, FileUser, HomeIcon, Layers, Mail } from "lucide-react";
 import Link from "next/link";
 import SignInButton from "../customs/SignInButton";
-
+import { useUserDetail } from "@/app/provider";
+import { Button } from "./button";
+import Image from "next/image";
+import { MdSpaceDashboard } from "react-icons/md";
 function Header() {
+  const { userDetail, setUserDetail } = useUserDetail();
   const [state, setState] = useState(false);
   return (
     <>
@@ -68,8 +61,26 @@ function Header() {
               state ? "" : "hidden"
             }`}
           >
-            <li className="order-2 pb-5 md:pb-0 flex gap-2">
-              <SignInButton />
+            <li className="order-2 pb-5 md:pb-0 flex gap-3">
+              {userDetail?.email ? (
+                <div className="flex items-center gap-2">
+                  <Link href={"/dashboard"}>
+                    <Button className="flex items-center gap-1 cursor-pointer hover:bg-slate-700 bg-slate-800 rounded-sm">
+                      Dashboard
+                      <MdSpaceDashboard />
+                    </Button>
+                  </Link>
+                  <Image
+                    src={userDetail?.picture}
+                    alt="user"
+                    width={38}
+                    height={38}
+                    className="rounded-full"
+                  />
+                </div>
+              ) : (
+                <SignInButton />
+              )}
             </li>
             <div className="order-1 bg-slate-900 p-3 md:p-0 rounded-md md:bg-transparent flex-1 justify-center items-center space-y-5 md:flex md:space-x-6 md:space-y-0">
               <Link href={"/"}>
