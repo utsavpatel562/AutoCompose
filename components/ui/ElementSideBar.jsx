71a8025254ebe52611cac651ddfaec6dbebb8096
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-04-15 16:06:33 -0400

    latest update

--- components/ui/ElementSideBar.jsx
+++ components/ui/ElementSideBar.jsx
@@ -1,15 +1,22 @@
 import Layout from "@/Data/Layout";
 import React from "react";
 import ElementLayoutCard from "./ElementLayoutCard";
+import ElementList from "@/Data/ElementList";
 
 function ElementSideBar() {
   return (
     <>
       <div className="p-5">
-        <h2 className="font-bold text-lg text-slate-800">Layouts</h2>
+        <h2 className="font-bold text-lg text-slate-700">Layouts</h2>
         <div className="grid grid-cols-3 md:grid-cols-2 gap-3 mt-3">
           {Layout.map((layout, index) => (
             <ElementLayoutCard layout={layout} key={index} />
+          ))}
+        </div>
+        <h2 className="font-bold text-lg text-slate-700 mt-4">Elements</h2>
+        <div className="grid grid-cols-3 md:grid-cols-2 gap-3 mt-3">
+          {ElementList.map((element, index) => (
+            <ElementLayoutCard layout={element} key={index} />
           ))}
         </div>
       </div>
